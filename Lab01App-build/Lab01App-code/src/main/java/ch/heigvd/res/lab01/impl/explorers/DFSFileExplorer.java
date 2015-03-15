448150fdd16a566d52877fbbb5499c052910dfc3
BafS <fabacrans@hotmail.com>
2015-03-15 23:43:38 +0100

    DFSFileExplorer completed

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/explorers/DFSFileExplorer.java
@@ -3,20 +3,38 @@ package ch.heigvd.res.lab01.impl.explorers;
 import ch.heigvd.res.lab01.interfaces.IFileExplorer;
 import ch.heigvd.res.lab01.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
+import org.apache.commons.io.comparator.NameFileComparator;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
- * node (file and directory). When the explorer reaches a directory, it visits all
- * files in the directory and then moves into the subdirectories.
- * 
+ * node (file and directory). When the explorer reaches a directory, it visits
+ * all files in the directory and then moves into the subdirectories.
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void explore(File rootDirectory, IFileVisitor visitor) {
+		visitor.visit(rootDirectory);
+
+		File[] files = rootDirectory.listFiles();
+
+		if(files != null) {
+			Arrays.sort(files, NameFileComparator.NAME_INSENSITIVE_COMPARATOR);
+			
+			for(File file : files) {
+				if(file.isFile()) {
+					visitor.visit(file);
+				} else if(file.isDirectory()) {
+					explore(file, visitor);
+				}
+			}
+		}
+
+//    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	}
 
 }
