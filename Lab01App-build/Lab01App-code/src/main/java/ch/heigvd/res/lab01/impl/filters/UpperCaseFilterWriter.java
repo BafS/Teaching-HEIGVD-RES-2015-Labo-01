efc8b132cd9b3801732e8e63b64edf24a30fe669
BafS <fabacrans@hotmail.com>
2015-03-16 00:12:07 +0100

    fix uppercase methods

--- Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
+++ Lab01App-build/Lab01App-code/src/main/java/ch/heigvd/res/lab01/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,27 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	public UpperCaseFilterWriter(Writer wrappedWriter) {
+		super(wrappedWriter);
+	}
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(String str, int off, int len) throws IOException {
+		out.write(str.toUpperCase(), off, len);
+//    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	}
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	@Override
+	public void write(char[] cbuf, int off, int len) throws IOException {
+		write(String.valueOf(cbuf), off, len);
+//		throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	}
+
+	@Override
+	public void write(int c) throws IOException {
+		write(Character.toChars(c), 0, 1);
+//		throw new UnsupportedOperationException("The student has not implemented this method yet.");
+	}
 
 }
